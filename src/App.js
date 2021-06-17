fe245d41cf951dc35db1c38ea95c7c9606ea2a13
imoempire <mrimoempire@gmail.com>
2021-06-17 13:36:26 +0000

    add body in app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Body from './Component/Body/Body';
 import Navbar from './Component/Navbar/Navbar';
 
 function App() {
   return (
    <>
    <Navbar />
-   
+   <Body />
    </>
   );
 }
