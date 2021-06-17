61c4ebe48008229200d12b9b85200edca35d5cd8
imoempire <mrimoempire@gmail.com>
2021-06-17 13:35:48 +0000

    add body component elements

---
+++ src/Component/Body/Body.jsx
@@ -0,0 +1,313 @@
+import React from 'react';
+import image1 from '../../images 2/un 10.png';
+import image2 from '../../images 2/un 11.png';
+import image3 from '../../images 2/un 4.png';
+import image4 from '../../images 2/un 6.png';
+import image5 from '../../images 2/un 8.png';
+import image6 from '../../images 2/Untitled-1.png';
+import image7 from '../../images 2/Untitled-12.png';
+import image8 from '../../images 2/Untitled-13.png';
+import image9 from '../../images 2/Untitled-14.png';
+import image10 from '../../images 2/Untitled-15.png';
+import image11 from '../../images 2/Untitled-16.png';
+import image12 from '../../images 2/Untitled-2.png';
+import image13 from '../../images 2/Untitled-3.png';
+import image14 from '../../images 2/Untitled-7.png';
+import image15 from '../../images 2/Untitled-9.png';
+
+
+
+
+
+
+
+
+const Body = () => {
+    return (
+        <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-2">
+            <div className="row">
+              <div className="col-md-3 size">XS</div>
+              <div className="col-md-3 size">S</div>
+              <div className="col-md-3 size">M</div>
+              <div className="col-md-3 size">ML</div>
+            </div>
+            <div className="row">
+              <div className="col-md-3 size">L</div>
+              <div className="col-md-3 size">XL</div>
+              <div className="col-md-3 size">XXL</div>
+            </div>
+            <p>Leave a star on the Github if this <br /> repository was useful</p>
+            <button type="button" className="btn btn-default btn-lg star">
+              <span className="glyphicon glyphicon-star" aria-hidden="true"></span>Star</button> 848
+          </div>
+    
+          <div className="col-md-10">
+            <div className="row">
+              <div className="col-md-3">
+                <div className="thumbnail">
+                  <button className=" btn text-right top"> Free Shipping</button>
+                  <img src={image1} alt="..." />
+                  <h3>Blue dots t-shirt</h3>
+                  <hr />
+                  <p className="center">
+                    $ <big >10</big>.90 <br />
+                    <small className="prize">or $ 9x1.21</small>
+                  </p>
+                  <div className="caption cap-content">
+                    <p>Add to Cart</p>
+                  </div>
+                </div>
+                
+              </div>
+              <div className="col-md-3">
+                <div className="thumbnail">
+                  <button className=" btn text-right top"> Free Shipping</button>
+                  <img src={image2} alt="..." />
+                  <h3>Red-long</h3>
+                  <hr />
+                  <p className="center">
+                    $ <big >23</big>.40 <br />
+                    <small className="prize">or $ 10x1.21</small>
+                  </p>
+                  <div className="caption cap-content">
+                    <p>Add to Cart</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="thumbnail">
+                  <button className=" btn text-right top"> Free Shipping</button>
+                  <img src={image3} alt="..." />
+                  <h3>playboy t-shirt</h3>
+                  <hr />
+                  <p className="center">
+                    $ <big >60</big>.90 <br />
+                    <small className="prize">or $ 15x1.21</small>
+                  </p>
+                  <div className="caption cap-content">
+                    <p>Add to Cart</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="thumbnail">
+                  <button className=" btn text-right top"> Free Shipping</button>
+                  <img src={image4} alt="..." />
+                  <h3>orange t-shirt</h3>
+                  <hr />
+                  <p className="center">
+                    $ <big >10</big>.90 <br />
+                    <small className="prize">or $ 9x1.21</small>
+                  </p>
+                  <div className="caption cap-content">
+                    <p>Add to Cart</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+    
+    
+            <div className="row">
+              <div className="col-md-3">
+                <div className="thumbnail">
+                  <button className=" btn text-right top"> Free Shipping</button>
+                  <img src={image5} alt="..." />
+    
+                  <h3>Mulit-t-shirt</h3>
+                  <hr />
+                  <p className="center">
+                    $ <big >10</big>.90 <br />
+                    <small className="prize">or $ 9x1.21</small>
+                  </p>
+                  <div className="caption cap-content">
+                    <p>Add to Cart</p>
+    
+                  </div>
+    
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="thumbnail">
+                  <button className=" btn text-right top"> Free Shipping</button>
+                  <img src={image6} alt="..." />
+                  <h3>Pink t-shirt</h3>
+                  <hr />
+                  <p className="center">
+                    $ <big >10</big>.90 <br />
+                    <small className="prize">or $ 9x1.21</small>
+                  </p>
+                  <div className="caption cap-content">
+                    <p>Add to Cart</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="thumbnail">
+                  <button className=" btn text-right top"> Free Shipping</button>
+                  <img src={image7} alt="..." />
+                  <h3>Grey t-shirt</h3>
+                  <hr />
+                  <p className="center">
+                    $ <big >10</big>.90 <br />
+                    <small className="prize">or $ 9x1.21</small>
+                  </p>
+                  <div className="caption cap-content">
+                    <p>Add to Cart</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="thumbnail">
+                  <button className=" btn text-right top"> Free Shipping</button>
+                  <img src={image8} alt="..." />
+                  <h3>Blue dragon t-shirt</h3>
+                  <hr />
+                  <p className="center">
+                    $ <big >10</big>.90 <br />
+                    <small className="prize">or $ 9x1.21</small>
+                  </p>
+                  <div className="caption cap-content">
+                    <p>Add to Cart</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+    
+            <div className="row">
+              <div className="col-md-3">
+                <div className="thumbnail">
+                  <button className=" btn text-right top"> Free Shipping</button>
+                  <img src={image9} alt="..." />
+                  <h3>Red b t-shirt</h3>
+                  <hr />
+                  <p className="center">
+                    $ <big >10</big>.90 <br />
+                    <small className="prize">or $ 9x1.21</small>
+                  </p>
+                  <div className="caption cap-content">
+                    <p>Add to Cart</p>
+                  </div>
+    
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="thumbnail">
+                  <button className=" btn text-right top"> Free Shipping</button>
+                  <img src={image10} alt="..." />
+                  <h3>Blue strips t-shirt</h3>
+                  <hr />
+                  <p className="center">
+                    $ <big >10</big>.90 <br />
+                    <small className="prize">or $ 9x1.21</small>
+                  </p>
+                  <div className="caption cap-content">
+                    <p>Add to Cart</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="thumbnail">
+                  <button className=" btn text-right top"> Free Shipping</button>
+                  <img src={image11} alt="..." />
+                  <h3>White t-shirt</h3>
+                  <hr />
+                  <p className="center">
+                    $ <big >10</big>.90 <br />
+                    <small className="prize">or $ 9x1.21</small>
+                  </p>
+                  <div className="caption cap-content">
+                    <p>Add to Cart</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="thumbnail">
+                  <button className=" btn text-right top"> Free Shipping</button>
+                  <img src={image12} alt="..." />
+                  <h3>Long grey</h3>
+                  <hr />
+                  <p className="center">
+                    $ <big >10</big>.90 <br />
+                    <small className="prize">or $ 9x1.21</small>
+                  </p>
+                  <div className="caption cap-content">
+                    <p>Add to Cart</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+    
+            <div className="row">
+              <div className="col-md-3">
+                <div className="thumbnail">
+                  <button className=" btn text-right top"> Free Shipping</button>
+                  <img src={image13} alt="..." />
+                  <h3> black-mult t-shirt</h3>
+                  <hr />
+                  <p className="center">
+                    $ <big >10</big>.90 <br />
+                    <small className="prize">or $ 9x1.21</small>
+                  </p>
+                  <div className="caption cap-content">
+                    <p>Add to Cart</p>
+    
+                  </div>
+    
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="thumbnail">
+                  <button className=" btn text-right top"> Free Shipping</button>
+                  <img src={image5} alt="..." />
+                  <h3>Muliti t-shirt</h3>
+                  <hr />
+                  <p className="center">
+                    $ <big >10</big>.90 <br />
+                    <small className="prize">or $ 9x1.21</small>
+                  </p>
+                  <div className="caption cap-content">
+                    <p>Add to Cart</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="thumbnail">
+                  <button className=" btn text-right top"> Free Shipping</button>
+                  <img src={image14} alt="..." />
+                  <h3>Black t-shirt</h3>
+                  <hr />
+                  <p className="center">
+                    $ <big >10</big>.90 <br />
+                    <small className="prize">or $ 9x1.21</small>
+                  </p>
+                  <div className="caption cap-content">
+                    <p>Add to Cart</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="thumbnail">
+                  <button className=" btn text-right top"> Free Shipping</button>
+                  <img src={image15} alt="..." />
+                  <h3>quote t-shirt</h3>
+                  <hr />
+                  <p className="center">
+                    $ <big >10</big>.90 <br />
+                    <small className="prize">or $ 9x1.21</small>
+                  </p>
+                  <div className="caption cap-content">
+                    <p>Add to Cart</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+    );
+}
+
+export default Body;
